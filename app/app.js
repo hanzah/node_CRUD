2d07894780833f3df7ba52519078c6f952da1c9e
Hani <Hani@Hanis-MacBook-Air.local>
2016-03-23 01:30:27 +0100

    Adding basic docerfile and docer compose yml

---
+++ app/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var app = express();
+
+mongoose.connect('mongodb://db:27017/meetup', function(err){
+	if(err){
+		console.log('Mongo connection error!');
+		console.log(err);
+	}else{
+		console.log('Mongo connected!');
+	}
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
