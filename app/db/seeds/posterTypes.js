43067b1cf2d32278f7d41fcdbdcfcef1759d1ab9
Hani <Hani@Hanis-MacBook-Air.local>
2016-04-19 17:42:11 +0200

    Adding seed poster data

--- app/db/seeds/posterTypes.js
+++ app/db/seeds/posterTypes.js
@@ -5,19 +5,74 @@ module.exports = function(){
 	console.log('Seeding Poster Types');
 	var posterTypes = [
 		{
-			name: 'name1',
-			photo: 'photo1',
-			icon: 'icon1'
+			name: 'Club',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme1.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon1.png'
 		},
 		{
-			name: 'name2',
-			photo: 'photo2',
-			icon: 'icon2'
+			name: 'Drink',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme2.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon2.png'
 		},
 		{
-			name: 'name3',
-			photo: 'photo3',
-			icon: 'icon3'
+			name: 'Social',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme3.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon3.png'
+		},
+		{
+			name: 'Business',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme4.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon4.png'
+		},
+		{
+			name: 'Birthday',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme5.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon5.png'
+		},
+		{
+			name: 'Theater',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme6.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon6.png'
+		},
+		{
+			name: 'Camping',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme7.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon7.png'
+		},
+		{
+			name: 'Romance',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme8.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon8.png'
+		},
+		{
+			name: 'Food',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme9.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon9.png'
+		},
+		{
+			name: 'Family',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme10.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon10.png'
+		},
+		{
+			name: 'Fun',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme11.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon11.png'
+		},
+		{
+			name: 'Walk',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme12.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon12.png'
+		},
+		{
+			name: 'Party',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme13.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon13.png'
+		},
+		{
+			name: 'Nature',
+			photo: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/themes/Theme14.png',
+			icon: 'https://s3.eu-central-1.amazonaws.com/networks.meetup/icons/ThemeIcon14.png'
 		}
 	];
 	var defer = Q.defer();
